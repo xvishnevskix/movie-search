399bd72c7499519aa3336ea38428335f1c03c50d
xvishnevskix <45593711+xvishnevskix@users.noreply.github.com>
2023-05-30 14:58:24 +0400

    section popular films complete

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import {FiFilm, FiMenu, FiHome, FiUser, FiTv, FiHeart, FiSearch} from 'react-ico
 import classNames from 'classnames'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { Search } from './Search'
+import { Search } from '../Search/Search'
 
 export const Header = () => {
 
@@ -24,9 +24,7 @@ export const Header = () => {
                     <div className={styles.burger}>
                         <FiMenu />
                     </div>
-                    <Link href="/">
-                        <a className={styles.logo}>Kinomore</a>
-                    </Link>
+                    <span className={styles.logo}>Kinomore</span>
                     <div className={styles.dropdown}>
                         <ul className={classNames('list-reset', styles.dropdownList)}>
                             {items.map(el => (
